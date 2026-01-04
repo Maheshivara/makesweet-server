8791c5ad5c77d252b97a1531c98a1e3028e2daf4
Maheshivara <gabrimath@gmail.com>
2026-01-04 19:10:07 -0300

    refactor: :fire: remove redundant code

--- server/utils/commands.go
+++ server/utils/commands.go
@@ -9,97 +9,78 @@ type CommandBuilder interface {
 	Billboard(imagePath string, outputPath string) *exec.Cmd
 	Flag(imagePath string, outputPath string) *exec.Cmd
 	HeartLocket(leftImagePath string, rightImagePath string, outputPath string) *exec.Cmd
+	ChristmasHeartLocket(leftImagePath string, rightImagePath string, outputPath string) *exec.Cmd
 	Circuit(imagePath string, outputPath string) *exec.Cmd
 	Bear(imagePath string, outputPath string) *exec.Cmd
 	Doll(imageLeftPath string, imageMidPath string, imageRightPath string, outputPath string) *exec.Cmd
+	CustomTemplate(templatePath string, imagePaths []string, outputPath string) *exec.Cmd
 }
 
 func NewCommandBuilder() CommandBuilder {
 	return &commandBuilder{}
 }
-func (*commandBuilder) Billboard(imagePath string, outputPath string) *exec.Cmd {
-	cmd := exec.Command(
-		"/reanimator",
-		"--zip",
-		"/makesweet/templates/billboard-cityscape.zip",
-		"--in",
-		imagePath,
-		"--gif",
-		outputPath,
-	)
+
+func (c *commandBuilder) Billboard(imagePath string, outputPath string) *exec.Cmd {
+	images := []string{imagePath}
+	cmd := c.reanimateUsingTemplate("/makesweet/templates/billboard-cityscape.zip", images, outputPath)
 
 	return cmd
 }
 
-func (*commandBuilder) Flag(imagePath string, outputPath string) *exec.Cmd {
-	cmd := exec.Command(
-		"/reanimator",
-		"--zip",
-		"/makesweet/templates/flag.zip",
-		"--in",
-		imagePath,
-		"--gif",
-		outputPath,
-	)
-
+func (c *commandBuilder) Flag(imagePath string, outputPath string) *exec.Cmd {
+	images := []string{imagePath}
+	cmd := c.reanimateUsingTemplate("/makesweet/templates/flag.zip", images, outputPath)
 	return cmd
 }
 
-func (*commandBuilder) HeartLocket(leftImagePath string, rightImagePath string, outputPath string) *exec.Cmd {
-	cmd := exec.Command(
-		"/reanimator",
-		"--zip",
-		"/makesweet/templates/heart-locket.zip",
-		"--in",
-		leftImagePath,
-		rightImagePath,
-		"--gif",
-		outputPath,
-	)
-
+func (c *commandBuilder) HeartLocket(leftImagePath string, rightImagePath string, outputPath string) *exec.Cmd {
+	images := []string{leftImagePath, rightImagePath}
+	cmd := c.reanimateUsingTemplate("/makesweet/templates/heart-locket.zip", images, outputPath)
 	return cmd
 }
 
-func (*commandBuilder) Circuit(imagePath string, outputPath string) *exec.Cmd {
-	cmd := exec.Command(
-		"/reanimator",
-		"--zip",
-		"/makesweet/templates/circuit-board.zip",
-		"--in",
-		imagePath,
-		"--gif",
-		outputPath,
-	)
-
+func (c *commandBuilder) ChristmasHeartLocket(leftImagePath string, rightImagePath string, outputPath string) *exec.Cmd {
+	images := []string{leftImagePath, rightImagePath}
+	cmd := c.reanimateUsingTemplate("/makesweet/templates/heart-locket-hat.zip", images, outputPath)
 	return cmd
 }
 
-func (*commandBuilder) Bear(imagePath string, outputPath string) *exec.Cmd {
-	cmd := exec.Command(
-		"/reanimator",
-		"--zip",
-		"/makesweet/templates/flying-bear.zip",
-		"--in",
-		imagePath,
-		"--gif",
-		outputPath,
-	)
-
+func (c *commandBuilder) Circuit(imagePath string, outputPath string) *exec.Cmd {
+	images := []string{imagePath}
+	cmd := c.reanimateUsingTemplate("/makesweet/templates/circuit.zip", images, outputPath)
 	return cmd
 }
 
-func (*commandBuilder) Doll(imageLeftPath string, imageMidPath string, imageRightPath string, outputPath string) *exec.Cmd {
-	cmd := exec.Command(
-		"/reanimator",
-		"--zip",
-		"/makesweet/templates/nesting-doll.zip",
-		"--in",
-		imageLeftPath,
-		imageMidPath,
-		imageRightPath,
-		"--gif",
-		outputPath,
-	)
-
+func (c *commandBuilder) Bear(imagePath string, outputPath string) *exec.Cmd {
+	images := []string{imagePath}
+	cmd := c.reanimateUsingTemplate("/makesweet/templates/flying-bear.zip", images, outputPath)
 	return cmd
+}
+
+func (c *commandBuilder) Doll(imageLeftPath string, imageMidPath string, imageRightPath string, outputPath string) *exec.Cmd {
+	images := []string{imageLeftPath, imageMidPath, imageRightPath}
+	cmd := c.reanimateUsingTemplate("/makesweet/templates/nesting-doll.zip", images, outputPath)
+	return cmd
+}
+
+func (c *commandBuilder) CustomTemplate(templatePath string, imagePaths []string, outputPath string) *exec.Cmd {
+	cmd := c.reanimateUsingTemplate(templatePath, imagePaths, outputPath)
+	return cmd
+}
+
+func (*commandBuilder) reanimateUsingTemplate(templatePath string, imagePaths []string, outputPath string) *exec.Cmd {
+	{
+		args := []string{
+			"/reanimator",
+			"--zip",
+			templatePath,
+			"--in",
+		}
+		args = append(args, imagePaths...)
+		args = append(args, "--gif", outputPath)
+
+		cmd := exec.Command(args[0], args[1:]...)
+
+		return cmd
+	}
 }
