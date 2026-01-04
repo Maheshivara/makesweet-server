09c21b34643f9e4e892a476b31616f0caa9366d9
Maheshivara <gabrimath@gmail.com>
2026-01-04 19:13:02 -0300

    feat: :sparkles: add heart locket christmas variant

--- server/handlers/heartLocket.go
+++ server/handlers/heartLocket.go
@@ -5,24 +5,33 @@ import (
 	"makesweet/utils"
 	"net/http"
 	"os"
+	"os/exec"
 
-	"github.com/charmbracelet/log"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 )
 
+type heartLocketVariant string
+
+const (
+	heartLocketVariantStandard  heartLocketVariant = "standard"
+	heartLocketVariantChristmas heartLocketVariant = "christmas"
+)
+
 // CreateHeartLocket
-// @Summary Create a heart locket gif
-// @Description Use image-lef and image-right files from form to make a opening heart locket gif
-// @Tags Gif
-// @Accept mpfd
-// @Param image-left formData file true "A png or jpg image to left half"
-// @Param image-right formData file true "A png or jpg image to right half"
-// @Produce json image/gif
-// @Success 200 {file} binary "Generated Gif"
-// @Fail 400 {string} "Fail to load images from formData"
-// @Fail 500 {string} "Fail to generate gif"
-// @Router /gif/heart-locket [post]
+//
+//	@Summary		Create a heart locket gif
+//	@Description	Use image-lef and image-right files from form to make a opening heart locket gif
+//	@Tags			Gif
+//	@Accept			mpfd
+//	@Param			image-left	formData	file	true	"A png or jpg image to left half"
+//	@Param			image-right	formData	file	true	"A png or jpg image to right half"
+//	@Param			variant		formData	string	false	"Variant of heart locket gif"	Enums(standard,christmas) default(standard)
+//	@Produce		json image/gif
+//	@Success		200	{file}		binary	"Generated Gif"
+//	@Failure		400	{string}	string	"Fail to load image from form"
+//	@Failure		500	{string}	string	"Fail to generate gif"
+//	@Router			/gif/heart-locket [post]
 func CreateHeartLocketGif(ctx *gin.Context) {
 	leftImageFilePath, err := utils.SaveImageFromContext(ctx, "image-left")
 	if err != nil {
@@ -50,21 +59,21 @@ func CreateHeartLocketGif(ctx *gin.Context) {
 	outputID := uuid.New()
 	outputFileName := fmt.Sprintf("%s.gif", outputID.String())
 	outputPath := fmt.Sprintf("%s/%s", destFolderPath, outputFileName)
-
-	heartLocketCreateCommand := utils.NewCommandBuilder().HeartLocket(leftImageFilePath, rightImageFilePath, outputPath)
-	err = heartLocketCreateCommand.Run()
+	variant := heartLocketVariant(ctx.DefaultPostForm("variant", "standard"))
+	commandBuilder := utils.NewCommandBuilder()
+	var command *exec.Cmd
+	switch variant {
+	case heartLocketVariantChristmas:
+		command = commandBuilder.ChristmasHeartLocket(leftImageFilePath, rightImageFilePath, outputPath)
+	default:
+		command = commandBuilder.HeartLocket(leftImageFilePath, rightImageFilePath, outputPath)
+	}
+	err = command.Run()
 	if err != nil {
-		log.Error("Heart locket gif make fail.", "err", err)
-		ctx.JSON(http.StatusInternalServerError, "Fail to create gif")
+		ctx.JSON(http.StatusInternalServerError, "Fail to generate gif")
 		return
 	}
 	defer os.Remove(outputPath)
 
-	_, err = os.Stat(outputPath)
-	if err != nil {
-		log.Error("Heart locket output check fail.", "err", err)
-		ctx.JSON(http.StatusInternalServerError, "Fail to create gif")
-		return
-	}
 	ctx.File(outputPath)
 }
