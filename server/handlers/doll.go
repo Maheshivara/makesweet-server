d931f2cbcd47d8a7a515e838f139b525db35fb34
Maheshivara <gabrimath@gmail.com>
2026-01-04 19:14:33 -0300

    docs: :memo: format and improve swag docs

--- server/handlers/doll.go
+++ server/handlers/doll.go
@@ -12,18 +12,19 @@ import (
 )
 
 // CreateHeartLocket
-// @Summary Create a nesting doll gif
-// @Description Use three images to create a nesting doll gif
-// @Tags Gif
-// @Accept mpfd
-// @Param image-left formData file true "A png or jpg image to the left doll"
-// @Param image-mid formData file true "A png or jpg image to the mid doll"
-// @Param image-right formData file true "A png or jpg image to the right doll"
-// @Produce json image/gif
-// @Success 200 {file} binary "Generated Gif"
-// @Fail 400 {string} "Fail to load images from formData"
-// @Fail 500 {string} "Fail to generate gif"
-// @Router /gif/nesting-doll [post]
+//
+//	@Summary		Create a nesting doll gif
+//	@Description	Use three images to create a nesting doll gif
+//	@Tags			Gif
+//	@Accept			mpfd
+//	@Param			image-left	formData	file	true	"A png or jpg image to the left doll"
+//	@Param			image-mid	formData	file	true	"A png or jpg image to the mid doll"
+//	@Param			image-right	formData	file	true	"A png or jpg image to the right doll"
+//	@Produce		json image/gif
+//	@Success		200	{file}		binary	"Generated Gif"
+//	@Failure		400	{string}	string	"Fail to load images from formData"
+//	@Failure		500	{string}	string	"Fail to generate gif"
+//	@Router			/gif/nesting-doll [post]
 func CreateDollGif(ctx *gin.Context) {
 	leftImageFilePath, err := utils.SaveImageFromContext(ctx, "image-left")
 	if err != nil {
